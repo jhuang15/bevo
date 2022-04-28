0a38c3cfad9ad3d4fe2b9de83937dda5c870806a
jhuang15 <jchuang1020@gmail.com>
2022-04-28 19:48:29 -0400

    CSS changes for landing page

--- controllers/inventories.js
+++ controllers/inventories.js
@@ -10,28 +10,19 @@ module.exports = {
 };
 
 function update (req, res) {
-  //console.log("Update:"+req.params.id);
-
-  // using find, findById, findOne makes app crash, 
-  //findbyidandupdate wont crash but wont do anything but pending
   Inventory.findByIdAndUpdate(req.params.id, req.body.id, {new:true}, function (err, inventory) {
     console.log('Error: '+err);
     inventory.brand = req.body.brand;
-    //Inventory.id(req.params.id) = req.parms.id;
     inventory.save(function(err) {
       res.redirect(`/inventories/${inventory._id}`);
-  });
+    });
   }); 
 }
 
-function deleteItem(req, res, next) {
-  //only findByIdAndDelete will work
+function deleteItem(req, res) {
   Inventory.findByIdAndDelete(req.params.id, function (err) {
-    //inventory.remove(req.params.id);
-     //inventory.save().then(function () {
       res.redirect('/inventories');
-    })
-  //});
+  })
 }
 
 function index(req, res) {
@@ -45,9 +36,6 @@ function show(req, res) {
     console.log(inventory);
     res.render('inventories/show', { title: 'Show Items', inventory})
   })
-  //Inventory.findById(req.params.id).exec(function (err, inventories){
-  //  res.render('inventories', { title: 'Item Details', inventories});
- // })
 }
 
 function create(req, res) {
@@ -55,12 +43,10 @@ function create(req, res) {
   inventory.save(function(err) {
     if (err) return console.log('Error'+inventory);
     console.log("Created:" +inventory);
-    //res.redirect(`/inventories/${inventory._id}`);
     res.redirect('inventories');
   });
 }
  
 function newItem(req, res) {
-  //res.render('inventories', { title: ' New Item' });
   res.render('inventories', { title: ' New Item' });
 }
